2dda65e3004bacb5ec14a76a741b0bddb3271752
Agas Yanpratama <agas.yanpratama@gmail.com>
2018-05-23 07:15:05 +0700

    checkstyle edit

--- src/main/java/enterkomputer/utilities/BarangParser.java
+++ src/main/java/enterkomputer/utilities/BarangParser.java
@@ -11,5 +11,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 public class BarangParser {
 }
