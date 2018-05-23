b55121175baf6190c5d3c9ec6a85436371689a1b
Agas Yanpratama <agas.yanpratama@gmail.com>
2018-05-23 16:35:06 +0700

    change echo controller test

--- src/test/java/advprog/example/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/EchoControllerTest.java
@@ -21,6 +21,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.IOException;
+
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
 public class EchoControllerTest {
@@ -39,7 +41,7 @@ public class EchoControllerTest {
     }
 
     @Test
-    void testHandleTextMessageEvent() {
+    void testHandleTextMessageEvent() throws IOException {
         MessageEvent<TextMessageContent> event =
                 EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
 
@@ -59,7 +61,7 @@ public class EchoControllerTest {
     }
 
     @Test
-    void testKategoriEnterKomputer() {
+    void testKategoriEnterKomputer() throws IOException{
         MessageEvent<TextMessageContent> event =
                 EventTestUtil.createDummyTextMessage("/enterkomputer listkategori");
         TextMessage reply = echoController.handleTextMessageEvent(event);
